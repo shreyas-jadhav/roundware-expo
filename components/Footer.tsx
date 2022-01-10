b0f87aa882d2b10e80353b875e87b50bea91b437
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-10 23:35:54 +0530

    basic footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Appbar } from "react-native-paper";
+const Footer = () => {
+  return (
+    <Appbar>
+      <Appbar.Action icon="information" />
+    </Appbar>
+  );
+};
+
+export default Footer;
