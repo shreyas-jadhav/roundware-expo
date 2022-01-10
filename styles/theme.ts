2133855490e445b67a877f7f3a1612243ed92cb7
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-10 23:36:29 +0530

    theme file

---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme,  } from "react-native-paper";
+
+
+
+export const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  
+    // Specify custom property in nested object
+  
+};
