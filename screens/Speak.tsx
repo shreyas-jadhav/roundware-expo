346c0a918c6e55821ef507ea98ab720291462203
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-10 23:36:23 +0530

    basic speak page

---
+++ screens/Speak.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SpeakScreen = () => {
+  return (
+    <View>
+      <Text>Speak</Text>
+    </View>
+  );
+};
+
+export default SpeakScreen;
